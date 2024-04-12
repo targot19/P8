1ebc5ee6d7363a7fd6d1c85e706750c1e1dd5590
Tobynatoren <tobynatoren11@gmail.com>
2024-04-12 10:13:56 +0200

    Added mastergridview for timeblock

--- javacalenderprojectp8/app/build.gradle.kts
+++ javacalenderprojectp8/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.recyclerview)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
