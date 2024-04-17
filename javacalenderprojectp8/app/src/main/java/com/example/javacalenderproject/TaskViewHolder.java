6d40761cad4a8e471a925dbdb5f34bfa6d955a2f
Tobynatoren <tobynatoren11@gmail.com>
2024-04-17 13:18:22 +0200

    empty adapter & and viewholder

---
+++ javacalenderprojectp8/app/src/main/java/com/example/javacalenderproject/TaskViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.javacalenderproject;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskViewHolder extends RecyclerView.ViewHolder {
+    public TaskViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
