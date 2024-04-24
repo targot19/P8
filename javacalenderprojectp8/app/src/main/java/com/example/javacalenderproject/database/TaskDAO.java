f3ad594a86e5dcb02e6c88f4cb240309a87b7594
Tobynatoren <tobynatoren11@gmail.com>
2024-04-24 11:25:15 +0200

    message

--- javacalenderprojectp8/app/src/main/java/com/example/javacalenderproject/database/TaskDAO.java
+++ javacalenderprojectp8/app/src/main/java/com/example/javacalenderproject/database/TaskDAO.java
@@ -10,18 +10,21 @@ import java.util.List;
 public interface TaskDAO {
 
     // method for creating tasks
-    @Query("INSERT INTO taskTable (taskType) VALUES ('New task')")
-    void create();
+   // @Query("INSERT INTO taskTable (taskType) VALUES ('New task')")
+    //void create();
 
     // method for getting all the tasks in the database
     @Query("SELECT * FROM taskTable")
     List<Task> getAllTasks();
 
     // method for saving tasks to database
-    @Query("UPDATE taskTable SET taskType = :taskName WHERE id = :id")
-    void save(String taskName, int id);
+  //  @Query("UPDATE taskTable SET taskType = :taskName WHERE id = :id")
+   // void save(String taskName, int id);
 
+    // Insert bruges til at proppe en task i databasen.
     @Insert
     void insertTask(Task task);
+
+
 
 }
